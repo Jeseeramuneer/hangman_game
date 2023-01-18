441b3f494f1ca88d841974212ea6258f4fb4e420
Jeseeramuneer <122545144+Jeseeramuneer@users.noreply.github.com>
2023-01-18 18:14:09 +0400

    Initial commit

---
+++ lib/utils.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+TextStyle retrostyl(double size,[Color? color,FontWeight? fw]){
+  return TextStyle(
+    fontFamily: "RetroGaming",
+    color: color,
+    fontSize: size,
+    fontWeight: fw,
+  );
+}
+List wordsList=['FLUTTER','ANDROID','DART','PHTHON'];
+List <String> letters=[
+  'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+];
+
+
